f6bef646f5baa3dccbc46889019b8d545fc0edd7
lasconic <lasconic@gmail.com>
2017-03-02 14:35:40 +0100

    add some more api calls

--- MuseScoreAPI/constants.py
+++ MuseScoreAPI/constants.py
@@ -1,9 +1,9 @@
 """
     Constants For All MuseScore Endpoints
     -----------------------------------
-    
+
     Version 1.0,EST API.
-    
+
     URLs for each endpoint are composed of the following pieces:
         PROTOCOL://{subdomain}.DOMAIN/VERSION/{resource}?{parameters}
 """
@@ -26,7 +26,7 @@ STREAMING_SOCKET_TIMEOUT = 90  # 90 seconds per Twitter's recommendation
 
 REST_SUBDOMAIN = 'api'
 
-REST_SOCKET_TIMEOUT = 5
+REST_SOCKET_TIMEOUT = 20
 
 REST_ENDPOINTS = {
         # resource:              ( method )
@@ -34,34 +34,35 @@ REST_ENDPOINTS = {
         'me/sets':               ('GET',),
         'me/scores':             ('GET',),
         'me/favorites':          ('GET',),
-        'me/activities':         ('GET',),
+        'me/groups':             ('GET',),
         'me/history':            ('GET',),
-        
-        'user/:PARAM':           ('GET', 'POST'),  # ID 
+
+        'user/:PARAM':           ('GET', 'POST'),  # ID
         'user/:PARAM/score':     ('GET',),         # ID
         'user/:PARAM/scores':    ('GET',),         # ID
-        'user/:PARAM/favorites': ('GET',),         # ID 
-        'user/:PARAM/followers': ('GET',),         # ID 
-        'user/:PARAM/following': ('GET',),         # ID 
+        'user/:PARAM/favorites': ('GET',),         # ID
+        'user/:PARAM/followers': ('GET',),         # ID
+        'user/:PARAM/following': ('GET',),         # ID
         'user/:PARAM/groups':    ('GET',),         # ID
         'user/:PARAM/follow':    ('GET',),         # ID
         'user/:PARAM/sets':      ('GET',),         # ID
+        'user/:PARAM/history':   ('GET',),         # ID
 
 
-        'score':                 ('GET', 'POST'),  
-        'score/:PARAM':          ('GET', 'DELETE'), # ID  
-        'score/:PARAM/time':     ('GET',),   # ID 
+        'score':                 ('GET', 'POST'),
+        'score/:PARAM':          ('GET', 'DELETE'), # ID
+        'score/:PARAM/time':     ('GET',),   # ID
         'score/:PARAM/space':    ('GET',),
-        'score/:PARAM/favorite': ('GET',),   # ID           
+        'score/:PARAM/favorite': ('GET',),   # ID
         'score/:PARAM/comments': ('GET',),   # ID
         'score/:PARAM/comment':  ('POST',),   # ID
         'score/:PARAM/update':   ('POST',),   # ID
 
-        'set/:PARAM':             ('GET', 'DELETE'),     # ID 
+        'set/:PARAM':             ('GET', 'DELETE'),     # ID
 
-        'groups':                 ('GET',),  
-        'groups/:PARAM':          ('GET', 'DELETE'),    # ID  
-        'groups/:PARAM/score':    ('GET',),              # ID 
+        'groups':                 ('GET',),
+        'groups/:PARAM':          ('GET', 'DELETE'),    # ID
+        'groups/:PARAM/score':    ('GET',),              # ID
 
         'resolve':                ('GET',)
 }
